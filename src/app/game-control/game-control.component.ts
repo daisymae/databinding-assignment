fa6572e389aa6cdfafbdcc389036ca4a46d3ac5f
daisymae <daisymae@orcadiafarm.com>
2018-04-25 21:34:16 -0500

    Steps 1-5
    Oops. Missed the new files for step 1, so adding now.
    Step 2: create buttons to start and stop the game
    Step 3: emit an event with an incremental number every second (using
    setInterval)
    Step 4: make event listenable outside the component
    Step 5: When stopping the game, no more events should be emitted
    (clearInterval)

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+@Output() gameStarted = new EventEmitter<number>();
+interval;
+lastNumber = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  startTheGame() {
+    console.log('start the game');
+    this.interval = setInterval(() => {
+      this.gameStarted.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000);
+
+  }
+
+  stopTheGame() {
+    clearInterval(this.interval);
+  }
+}
